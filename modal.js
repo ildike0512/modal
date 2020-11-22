f4eba989161b59a71c3d50ac8d81f8bb8067512d
ildike0512 <hajos.ildiko007@gmail.com>
2020-11-22 16:38:01 +0100

    See it

--- modal.js
+++ modal.js
@@ -1,18 +1,27 @@
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
+'use script';
+
+let modal = document.querySelector('.modal');
+let trigger = document.querySelector('.trigger');
+let closeButton = document.querySelector('.close-button');
+(function clickBtnHandler(){
+    const buttons = document.querySelectorAll('.button');
+    for (let i=0; i< buttons.length; i++){
+        buttons[i].addEventListener('click', toggleModal)
+    }
+})();
+
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
-}
+};
 
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal();
     }
-}
+};
 
-trigger.addEventListener("click", toggleModal);
+trigger.addEventListener('click', toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
@@ -22,5 +31,7 @@ window.addEventListener("click", windowOnClick);
 
 
 
-/* 'use script';*/
+
+
+
 
